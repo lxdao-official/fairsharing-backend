2b17e26fefc36de36a94dfde0de1998e2ef0ae23
KieSun <zx597813039@gmail.com>
2023-08-27 15:56:53 +0800

    feat: add ValidationPipe transform

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,11 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+    }),
+  );
   await app.listen(3000);
 
   app.enableShutdownHooks();
