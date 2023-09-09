cbea83c2c49c9c523f72a77ecbcb53adf0a87e36
Kahn <ylkk925@gmail.com>
2023-09-10 01:13:36 +0800

    feat: set PORT

--- src/main.ts
+++ src/main.ts
@@ -21,7 +21,7 @@ async function bootstrap() {
       transformOptions: { enableImplicitConversion: true },
     }),
   );
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 
   app.enableShutdownHooks();
 }
