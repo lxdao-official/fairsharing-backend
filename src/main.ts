92b9588756b322ff38ffef089b902784f13ef4f9
KieSun <zx597813039@gmail.com>
2023-09-11 13:11:55 +0800

    feat: cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,9 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: true,
+  });
 
   const config = new DocumentBuilder()
     .setTitle('Cats example')
